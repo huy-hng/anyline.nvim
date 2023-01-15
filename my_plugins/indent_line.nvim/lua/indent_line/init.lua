fdd99274483a7ea8071e3b49813b3acf32b8c16b
Huy <huy@hoang.dev>
2023-01-15 13:50:29 +0100

    refactoring and extend context to startline

--- my_plugins/indent_line.nvim/lua/indent_line/init.lua
+++ my_plugins/indent_line.nvim/lua/indent_line/init.lua
@@ -1,5 +1,9 @@
 local M = {}
 
+-- TODO: when text changed, only update change area
+-- TODO: implement debounce logic. see /home/huy/.local/share/nvim/lazy/nvim-ufo/lua/ufo/lib/debounce.lua
+-- for reference
+
 ----------------------------------------------Config------------------------------------------------
 local opts = {
 	indent_char = '▏',
@@ -10,14 +14,22 @@ local opts = {
 	},
 	highlight = 'Comment',
 	context_highlight = 'ModeMsg',
-	priority = 1,
+	priority = 19,
 	priority_context = 20,
 }
 
 local namespace = vim.api.nvim_create_namespace('IndentLine')
 local namespace_context = vim.api.nvim_create_namespace('IndentLineContext')
 
----type table<number, table<lines: table, line_ranges: table>>
+---@alias line number
+---@alias column number
+
+---@alias line_cache table<line, column[]>
+---@alias range_cache table<column, number[]> { column: { start, end } }
+
+---@alias bufnr number
+
+---@type table<bufnr, { lines: line_cache, line_ranges: range_cache }>
 local buffer_caches = {}
 
 function M.setup(user_opts)
@@ -27,6 +39,7 @@ function M.setup(user_opts)
 	M.start()
 end
 
+--- start indentline autocmds
 function M.start()
 	Augroup('IndentLine', {
 		Autocmd({ 'CursorMoved', 'CursorMovedI' }, M.set_context),
@@ -44,6 +57,7 @@ function M.start()
 	})
 end
 
+-- stop autocmds
 function M.stop() DeleteAugroup('IndentLine') end
 
 local function get_treesitter(bufnr)
@@ -94,15 +108,17 @@ local function get_indent_width(bufnr)
 	return indent_width
 end
 
-local function convert_cache_format(bufnr)
+local function convert_cache_format(cached_lines)
+	---@type table<column, line[]>
 	local converted = {}
-	for line, columns in pairs(buffer_caches[bufnr].lines) do
+	for line, columns in pairs(cached_lines) do
 		for _, column in ipairs(columns) do
 			if not converted[column] then converted[column] = {} end
 			table.insert(converted[column], line)
 		end
 	end
 
+	---@type range_cache
 	local ranges = {}
 	for column, lines in pairs(converted) do
 		local start
@@ -125,37 +141,51 @@ local function convert_cache_format(bufnr)
 		end
 		table.insert(ranges[column], { start, last_line })
 	end
-	buffer_caches[bufnr].line_ranges = ranges
+
+	return ranges
 end
 
-local function cache_lines(bufnr, start, stop)
-	local _, ts_indent = get_treesitter(bufnr)
-	local indent_width = get_indent_width(bufnr)
-	local lines = vim.api.nvim_buf_get_lines(bufnr, start or 0, stop or -1, false)
-
+local function cache_lines(ts_indent, indent_width, lines)
 	local cached_lines = {}
+	-- loop through lines
 	for linenr, line_text in ipairs(lines) do
 		cached_lines[linenr] = {}
 
+		-- if indent in line is 0, skip line
 		local total_indent = ts_indent.get_indent(linenr)
 		if total_indent == 0 then
 			goto continue
 		end
 
-		local indent_depth = total_indent / indent_width
-		linenr = linenr - 1
+		local indents = total_indent / indent_width
 
-		for i = 0, indent_depth - 1 do
+		-- loop through indentation columns
+		for i = 0, indents - 1 do
 			local line_column = i * indent_width
 
 			if should_set_line(line_text, line_column + 1) then
-				table.insert(cached_lines[linenr], line_column)
+				table.insert(cached_lines[linenr - 1], line_column)
 			end
 		end
 
 		::continue::
 	end
-	buffer_caches[bufnr] = { lines = cached_lines }
+
+	return cached_lines
+end
+
+local function update_cache(bufnr, start, stop)
+	local _, ts_indent = get_treesitter(bufnr)
+	local indent_width = get_indent_width(bufnr)
+	local lines = vim.api.nvim_buf_get_lines(bufnr, start or 0, stop or -1, false)
+
+	local cached_lines = cache_lines(ts_indent, indent_width, lines)
+
+	local converted = convert_cache_format(cached_lines)
+	buffer_caches[bufnr] = {
+		lines = cached_lines,
+		line_ranges = converted,
+	}
 end
 
 local function set_lines(bufnr)
@@ -172,51 +202,69 @@ local function clear_lines(bufnr, ns) --
 end
 
 function M.detect_changes(data)
-	-- local start = vim.fn.getpos("'[")
-	-- local stop = vim.fn.getpos("']")
-
 	local bufnr = data.buf
 	clear_lines(bufnr, namespace)
 
-	cache_lines(bufnr)
-	convert_cache_format(bufnr)
+	update_cache(bufnr)
 
 	M.set_context(data)
 	set_lines(bufnr)
-
 end
 
-function M.set_context(data)
-	local bufnr = data.buf
-	if not buffer_caches[bufnr] then M.detect_changes(data) end
+local function get_indentation(bufnr, line)
+	local indents = buffer_caches[bufnr].lines[line]
+	local column = npcall(table.slice, indents, -1)
+	return column or -1
+end
 
+local function get_current_context(bufnr)
 	local cursor_pos = vim.fn.getcurpos(0)
 	local cursor_line = cursor_pos[2] - 1
-	local indents = buffer_caches[bufnr].lines[cursor_line]
-	local biggest_indent = npcall(table.slice, indents, -1)
-	clear_lines(bufnr, namespace_context)
-	if not biggest_indent then return end
 
-	local lines = buffer_caches[bufnr].line_ranges[biggest_indent]
+	local column = get_indentation(bufnr, cursor_line)
+	local next = get_indentation(bufnr, cursor_line + 1)
+
+	if not column and not next then return end
+	
+	-- include context when cursor is on start of context (not inside indentation yet)
+	if next > column then
+		column = next
+		cursor_line = cursor_line + 1
+	end
+
+	local lines = buffer_caches[bufnr].line_ranges[column]
+	if not lines then return end
+
 	for _, line_pair in ipairs(lines) do
 		local start = line_pair[1]
 		local stop = line_pair[2]
-		if cursor_line >= start and cursor_line <= stop then
-			for line = start, stop do
-				set_mark(
-					namespace_context,
-					bufnr,
-					line,
-					biggest_indent,
-					'IndentLineContext',
-					opts.priority_context
-				)
-			end
-			break
+
+		if cursor_line >= start and cursor_line <= stop then --
+			return start, stop, column
 		end
 	end
 end
 
--- M.setup()
+local function set_line(bufnr, start, stop, column)
+	for line = start, stop do
+		set_mark(namespace_context, bufnr, line, column, 'IndentLineContext', opts.priority_context)
+	end
+end
+
+function M.set_context(data)
+	local bufnr = data.buf or vim.api.nvim_get_current_buf()
+
+	if not buffer_caches[bufnr] then M.detect_changes(data) end
+
+	local start, stop, column = get_current_context(bufnr)
+
+	clear_lines(bufnr, namespace_context)
+	if not column then return end
+
+	set_line(bufnr, start, stop, column)
+end
+
+M.setup()
 -- M.stop()
+
 return M
