4de38ff4904469fc0b142425e7485fc8c5566c7d
Huy <huy@hoang.dev>
2023-02-28 15:15:52 +0100

    adjust opts

--- lua/anyline/opts.lua
+++ lua/anyline/opts.lua
@@ -11,10 +11,10 @@ M.opts = {
 	debounce_time = 30, -- how responsive to make to make the cursor movements (in ms, very low debounce time is kinda janky at the moment)
 	fps = 30, -- changes how many steps are used to transition from one color to another
 	fade_duration = 200, -- color fade speed (only used when lines_per_second is 0)
-	length_acceleration = 0.05, -- increase animation speed depending on how long the context is
+	length_acceleration = 0.02, -- increase animation speed depending on how long the context is
 
-	lines_per_second = 50, -- how many lines/seconds to show
-	trail_length = 10, -- how long the trail / fade transition should be
+	lines_per_second = 60, -- how many lines/seconds to show
+	trail_length = 20, -- how long the trail / fade transition should be
 
 	-- other stuff
 	priority = 19, -- extmark priority
@@ -27,6 +27,9 @@ M.opts = {
 }
 
 function M.parse_opts(opts)
+	if opts.animation == 'none' then --
+		M.opts.debounce_time = 0
+	end
 	M.opts = vim.tbl_extend('force', M.opts, opts)
 end
 
